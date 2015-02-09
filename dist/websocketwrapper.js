ed1cfc7dbfd00ceb7ecd30a866d9cb2cb93f5a0a
Iñaki Baz Castillo <ibc@aliax.net>
2015-02-09 23:08:27 +0100

    0.1.7

--- dist/websocketwrapper.js
+++ dist/websocketwrapper.js
@@ -175,7 +175,7 @@ function _onerror(e) {
 
 
 function _onclose(e) {
-	debugerror('onclose() | event: %o', e);
+	debug('onclose() | [code:%s, reason:%s, wasClean:%s', e.code, e.reason, e.wasClean);
 
 	var self = this;
 
